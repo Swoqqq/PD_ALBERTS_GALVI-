61fc3b9d98c212d04b4362990fef9090de1c223d
Swoqqq <agalvins2@edu.riga.lv>
2026-03-04 14:37:54 +0200

    izveidota def ievade

---
+++ ievarijuma_kalkulators.py
@@ -0,0 +1,14 @@
+def ievade():
+    abolu_svars=float(input('Ievadiet abolu svaru kilogramos:'))
+    if abolu_svars<0:
+        print("Kļuda")
+
+def aprekins(abolu svars):
+    
+    
+def izmaksa():
+    abolu_cena=float(input('Ievadiet abolu cenu eiros:'))
+    cukura_cena=float(input('Ievadiet cukura cenu eiros:'))
+    if cukura_cena<0 or abolu cena<0:
+        print("Kļuda")
+    
