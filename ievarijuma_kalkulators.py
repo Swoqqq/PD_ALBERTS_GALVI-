6ce332bb40b4d67ab1e9ad6b87231a82cac1e09a
Swoqqq <agalvins2@edu.riga.lv>
2026-03-04 14:39:14 +0200

    dalēji izpildīta funkcija izmaksa

--- ievarijuma_kalkulators.py
+++ ievarijuma_kalkulators.py
@@ -4,11 +4,12 @@ def ievade():
         print("Kļuda")
 
 def aprekins(abolu svars):
-    
+
     
 def izmaksa():
     abolu_cena=float(input('Ievadiet abolu cenu eiros:'))
     cukura_cena=float(input('Ievadiet cukura cenu eiros:'))
     if cukura_cena<0 or abolu cena<0:
         print("Kļuda")
+    
     
